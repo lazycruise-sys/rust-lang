35d48db412749730a01d112512af67bb8adb9122
lazycruise <gatesworkline@gmail.com>
2021-11-08 23:37:57 +0100

    update Main Rust File

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 // mod print;
 // mod vars;
-mod types;
+// mod types;
+// mod strings;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+// mod conditionals;
+// mod loops;
+// mod functions;
+// mod pointers;
+mod structs;
 
 fn main() {
     // module_name.run();
-    types::run();
+    structs::run();
 }
